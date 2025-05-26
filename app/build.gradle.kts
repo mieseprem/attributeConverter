d0905cf16c1b758b0ff1522ed7e4e5f4e9f64256
Clemens Nafe <clemens.nafe@otto.de>
2025-05-26 21:57:13 +0200

    add gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
+
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.5"
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform(SpringBootPlugin.BOM_COORDINATES))
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
